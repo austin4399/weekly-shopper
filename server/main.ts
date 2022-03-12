62cfebebad1cfa73c661216e3b41e360f65da2d4
austin4399 <Austin4399@gmail.com>
2022-03-12 17:18:56 -0500

    added dummy endpoint for parsing data

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
 const server = express();
+server.use(express.json());
 
 server.get('/test', (req, res) => {
     res.send('Hello World');
+})
+
+server.post('/test', (req, res) => {
+    res.send(req.body);
 })
 
 export default server
