a2af55cd2d65b8052a037aa16c39f0e2a8c1b5c8
austin4399 <Austin4399@gmail.com>
2022-03-13 17:18:43 -0400

    a few dummy routes

--- server/api/endpoints/v1/routes.ts
+++ server/api/endpoints/v1/routes.ts
@@ -1,9 +1,17 @@
-import express from 'express';
+import express, {Request, Response, Express} from 'express';
 
 const router = express.Router();
 
-router.use('/test', (req, res) => {
+router.get('/test', (req: Request, res: Response) => {
     res.send('test endpoint');
+})
+
+router.get('/ingredient', (req: Request, res: Response) => {
+    res.send('ingredient endpoint');
+});
+
+router.get('/connect', async (req: Request, res: Response) => {
+    res.send('connected to mongo');
 })
 
 export default router
