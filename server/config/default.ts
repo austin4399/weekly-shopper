fc4f8e6729e577df4afb5f9047921030271fc620
austin4399 <Austin4399@gmail.com>
2022-03-13 11:50:12 -0400

    removed unnecessary modules

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,8 +1,5 @@
 import 'dotenv/config';
 
 export default {
-    port: '80',
-    host: 'localhost',
-    dbUri: `mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@cluster0.u1kdy.mongodb.net/typescript-api-demo?retryWrites=true&w=majority`,
-    saltWorkFactor: 10,
+    dbUri: `mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@cluster0.u1kdy.mongodb.net/weekly-shopper-db?retryWrites=true&w=majority`,
 }
