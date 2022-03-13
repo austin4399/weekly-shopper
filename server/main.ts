2e3f6e5e2e25b542b98b081517d1c7835cfa3a53
austin4399 <Austin4399@gmail.com>
2022-03-13 17:42:56 -0400

    added date formatting to connection log

--- server/main.ts
+++ server/main.ts
@@ -1,17 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
-import 'dotenv/config'
 import mongoose from 'mongoose';
 import routesv1 from './api/endpoints/v1/routes';
+import dayjs from 'dayjs';
 
 const server = express();
+
 const dbUri: string = `mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@dev.ivzgx.mongodb.net/dev`;
+
 mongoose.connect(dbUri)
-.then(() => {
-    console.log("Database is connected");
-})
-.catch(err => {
-    console.log({ database_error: err });
-});
+    .then(() => {
+        console.info(`[${dayjs()}] Connected to MongoDB`);
+    }).catch(error => {
+        console.error(error);
+    });
+
+
 server.use(express.json());
 server.use('/v1', routesv1);
 
