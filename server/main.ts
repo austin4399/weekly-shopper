f4ee161f317145e892d8eca9c06697cca5773f9a
austin4399 <Austin4399@gmail.com>
2022-03-13 17:18:21 -0400

    added mongo connection to server

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import 'dotenv/config'
+import mongoose from 'mongoose';
 import routesv1 from './api/endpoints/v1/routes';
 
 const server = express();
-
+const dbUri: string = `mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@dev.ivzgx.mongodb.net/dev`;
+mongoose.connect(dbUri)
+.then(() => {
+    console.log("Database is connected");
+})
+.catch(err => {
+    console.log({ database_error: err });
+});
 server.use(express.json());
-
 server.use('/v1', routesv1);
 
 export default server
