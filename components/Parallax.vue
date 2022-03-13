73f8d4c9117e760e16e437065c2e13bb2e2361f0
mo17817 <lazerharry@gmail.com>
2022-03-13 17:22:21 -0400

    got rid of old layout added P-lax

---
+++ components/Parallax.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-parallax src="~assets/mealwheel.jpg" height="500" width="auto">
+    <v-row>
+      <v-col align = "center" class = "text-white">
+          <v-title>
+            Weekly Meals on a Tap
+          </v-title>
+          <lb/>
+
+          <h3>
+            Click the button for a randomly generated recipe
+          </h3>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  name: 'Parallax'
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template>
+<v-app>
+
+ <h1>
+   empty
+ </h1>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: 'empty'
+}
+</script>
+
+<style>
+
+</style>
