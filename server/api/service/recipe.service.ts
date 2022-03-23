6f548b423ad8b3870e4a2f1584b38fa3130916ae
auhallet <austin.hallett-doughty@silabs.com>
2022-03-23 19:44:54 -0400

    find random recipe service

--- server/api/service/recipe.service.ts
+++ server/api/service/recipe.service.ts
@@ -1,5 +1,6 @@
-import { DocumentDefinition } from "mongoose";
+import { FilterQuery, QueryOptions, DocumentDefinition } from "mongoose";
 import RecipeModel, { RecipeDocument } from "../models/recipe.model";
+// import { databaseResponseTimeHistogram } from "../utils/metrics";
 
 export async function createRecipe(
   input: DocumentDefinition<Omit<RecipeDocument, "createdAt" | "updatedAt">>
@@ -10,4 +11,42 @@ export async function createRecipe(
   } catch (e: any) {
     throw new Error(e);
   }
+}
+
+export async function findRecipe(
+  query: FilterQuery<RecipeDocument>,
+  options: QueryOptions = { lean: true }
+) {
+  const metricsLabels = {operation: "findProduct",};
+
+  // const timer = databaseResponseTimeHistogram.startTimer();
+  try {
+    const result = await RecipeModel.findOne(query, {}, options);
+    // timer({ ...metricsLabels, success: "true" });
+    return result;
+  } catch (e: any) {
+    // timer({ ...metricsLabels, success: "false" });
+    throw new Error(e);
+  }
+}
+
+// Get the count of all users
+export async function findRandomRecipe(){
+  try {
+      RecipeModel.count().exec(function(err: any, count: number) {
+      
+      // Get a random entry
+      var random = Math.floor(Math.random() * count)
+  
+      // Again query all users but only fetch one offset by our random #
+      RecipeModel.findOne().skip(random).exec(
+        function (err, result) {
+          // Tada! random user
+          console.log(result) 
+        })
+    })   
+  } catch (e: any) {
+    throw new Error(e);
+  }
+
 }
