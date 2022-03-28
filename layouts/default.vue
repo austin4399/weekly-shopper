c432929ed2af64eb88c6a647a577b3c032ddbd36
mo17817 <lazerharry@gmail.com>
2022-03-28 15:40:20 -0400

    required packages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,6 @@
 <template>
   <v-app dark align = "center">
-    <v-card class ="overflow-hidden d-inline-flex flex-center" style="align-items: center;">
-      <v-app-bar
-      color="primary"
-      dark
-      shrink-on-scroll
-      src="https://picsum.photos/1920/1080?random"
-      scroll-target="#scrolling-techniques-2"
-      fade-img-on-scroll
-      floating
-      >
-       <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
-      <v-app-bar-title id = "app-bar-title" class="flex flex-center" style="align-items: center;" align = "center">
-          Weekly shopper
 
-      </v-app-bar-title>
-      </v-app-bar>
-
-    </v-card>
     <v-main>
         <Nuxt />
     </v-main>
@@ -58,7 +36,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Weekly-shopper',
+      title: 'Vuetify.js',
     }
   },
 }
