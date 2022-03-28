d88ea70d30118fc240eee2bc0998b5fdbe088a4e
auhallet <austin.hallett-doughty@silabs.com>
2022-03-28 19:08:09 -0400

    moved tab > layout/ changed to class TS component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,61 @@
 <template>
-  <v-app dark align = "center">
+  <v-app>
+    <v-app-bar
+      app
+      color="flex flex-around grey darken-1"
+      flat
+      rounded
+    >
+      <div
+        :color="
+          $vuetify.breakpoint.smAndDown ? 'light-grey lighten-3' : 'transparent'
+        "
+        size="32"
+      ></div>
+      <v-tabs grow centered class="ml-n9" color="black darken-1">
+        <!-- path to weekly cart -->
+        <v-tab left class="flex flex-between" to="/">
+          <v-icon class="pa-md-sm"> mdi-cart-outline </v-icon>
+        </v-tab>
+        <!-- path to other pages -->
+        <v-tab v-for="link in links" :key="link" :to="link.to">
+          {{ link.name }}
+        </v-tab>
+      </v-tabs>
+
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+      ></v-avatar>
+    </v-app-bar>
 
     <v-main>
         <Nuxt />
     </v-main>
+    
   </v-app>
 </template>
 <style>
-#app-bar-title{
-    display: center;
-    align-items: center;
-}
+
 </style>
 
-<script>
-export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class DefaultLayout extends Vue {
+  links: any = [
+    {
+      name: 'Build a Cart',
+      to: '/weeklycart',
+      id: 'ran-rec',
     }
-  },
+  ]
+
+  clipped: boolean = false;
+  drawer: boolean = false;
+  fixed: boolean = false;
+
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,33 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      app
-      color="flex flex-between grey darken-1"
-      flat
-      rounded
-      id="inspire"
-    >
-      <div
-        :color="
-          $vuetify.breakpoint.smAndDown ? 'light-grey lighten-3' : 'transparent'
-        "
-        size="32"
-      ></div>
-      <v-tabs grow centered class="ml-n9" color="black darken-1" id="hover">
-        <!-- path to weekly cart -->
-        <v-tab left class="flex flex-between" to="/weeklycart">
-          <v-icon class="pa-md-sm"> mdi-cart-outline </v-icon>
-        </v-tab>
-        <!-- path to other pages -->
-        <v-tab v-for="link in links" :key="link" :to="link.to">
-          {{ link.name }}
-        </v-tab>
-      </v-tabs>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
-
-    <v-main class="grey lighten-3">
+    <v-main class="grey lighten-3" style="height: 100%;">
       <v-container>
         <v-row>
-          <v-col cols="12" sm="8" align="center" class="flex flex-center">
+          <v-col cols="12" sm="8" align="center">
             <v-sheet rounded="lg" class="flex flex-center justify-center">
               <v-img src="https://picsum.photos/1900/1080"> </v-img>
-              <!--  -->
             </v-sheet>
           </v-col>
           <v-col>
             <v-bottom-sheet v-model="sheet" inset>
               <template v-slot:activator="{ on, attrs }">
-                <v-btn color="grey darken-1" dark v-bind="attrs" v-on="on">
-                  Open Calculator
-                </v-btn>
+                <v-col align="center">
+                  <v-btn 
+                    color="grey darken-1" 
+                    dark 
+                    v-bind="attrs" 
+                    v-on="on"
+                  >
+                    Open Calculator
+                  </v-btn>
+                </v-col>
+                
               </template>
               <v-sheet class="text-center" height="auto" persistent>
-                <v-btn class="mt-6" text color="error" @click="sheet = !sheet">
+                <v-btn 
+                  class="mt-6" 
+                  text 
+                  color="error" 
+                  @click="sheet = !sheet">
                 </v-btn>
                 <div class="my-3">
                   <script
@@ -67,42 +46,12 @@
         </v-row>
       </v-container>
     </v-main>
-  </v-app>
 </template>
 
-<style>
-#hover:hover {
-  background-color: grey;
-}
-@media screen and (max-width: 400px) {
-  #main-container {
-    display: flex;
-    size: auto;
-    overflow: hidden;
-  }
-}
-</style>
-
-<script>
-import { mdiAllInclusiveBox, mdiCartOutline } from '@mdi/js'
-export default {
-  name: 'IndexPage',
-  layout: 'none',
-  data() {
-    return {
-      links: [
-        {
-          name: 'Random Recipe List',
-          to: '/weeklycart',
-          id: 'ran-rec',
-        },
-        {
-          name: 'Ordered Recipes',
-          to: '/weeklycart',
-          id: 'ordered-rec',
-        },
-      ],
-    }
-  },
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component({})
+export default class IndexPage extends Vue {
 }
 </script>
