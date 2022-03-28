b9a60ab2d9fe0e369722e1912a50a3dedd11e9d1
mo17817 <lazerharry@gmail.com>
2022-03-28 15:39:34 -0400

    new homepage layout for cleaner look

--- pages/index.vue
+++ pages/index.vue
@@ -1,166 +1,116 @@
 <template>
-  <v-main id="main-container" shrink class="overflow-hidden">
-    <div class="bg-transparent col" align="center">
-      <!-- will add pictures to parallax when i figure out how to load static files  -->
-
-      <v-parallax
-        dark
-        class="overflow-hidden"
-        src="https://picsum.photos/1900/1080?random"
-        align="center"
-        id="parallax"
-        shrink
+  <v-app id="inspire">
+    <v-app-bar app color="flex flex-between grey darken-1" flat rounded id="inspire">
+      <div
+        :color="
+          $vuetify.breakpoint.smAndDown ? 'light-grey darken-3' : 'transparent'
+        "
+        size="32"
       >
-        <v-row align="center" class="d-flex flex-center text-black">
-          <v-col align="inherit" id="wheel-txt">
-            <v-title> Weekly Recipes Made Easy </v-title>
-            <h3>Click below to find a recipe for you</h3>
+      </div>
+      <v-tabs grow centered class="ml-n9" color="black darken-1">
+              <!-- path to weekly cart -->
+        <v-tab left class="flex flex-between" to="/weeklycart">
+          <v-icon class="pa-md-sm"> mdi-cart-outline </v-icon>
+        </v-tab>
+        <v-tab v-for="link in links" :key="link" :to="link.to">
+          {{ link.name }}
+        </v-tab>
+      </v-tabs>
+
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+      ></v-avatar>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="8" align="center" class="flex flex-center">
+            <v-sheet rounded="lg" class="flex flex-center justify-center">
+              <v-img src="https://picsum.photos/1900/1080"> </v-img>
+              <!--  -->
+            </v-sheet>
+          </v-col>
+          <v-col cols="12" sm="4" align="center">
+            <v-sheet rounded="lg" class="flex flex-center">
+              <v-card
+                class="blue-grey lighten-5 flex-colum d-flex justify-start overflow-hidden pa-md"
+                justify="start"
+                id="first-card"
+              >
+                <span class="text-h5 black--text" align="center">
+                  <!-- random list of recipes -->
+                  <v-icon large color="black"> mdi-all-inclusive </v-icon>
+                </span>
+                <v-spacer></v-spacer>
+                <v-menu bottom left>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn dark icon v-bind="attrs" v-on="on">
+                      <v-icon class="black--text">mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-title> recipes </v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-card>
+              <!---->
+            </v-sheet>
           </v-col>
         </v-row>
-      </v-parallax>
-    </div>
-    <!-- food calculator -->
-    <v-spacer></v-spacer>
-    <div
-      class="flex-row d-flex d-inline-flex flex-center overflow-hidden"
-      color="primary"
-      justify="space-around"
-      align="center"
-      id="card-container"
-    >
-      <v-card
-        class="blue-grey lighten-5 flex-colum d-flex justify-start black--text overflow-hidden"
-        justify="start"
-        id="first-card"
-        width="400"
-      >
-        <span class="text-h5">
-          <!-- generated list of recipes  -->
-          <v-icon large color="black">
-            mdi-format-list-bulleted
-          </v-icon>
-        </span>
-        <v-spacer></v-spacer>
-        <v-menu bottom left>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn dark icon v-bind="attrs" v-on="on">
-              <v-icon class="black--text">mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item>
-              <v-list-item-title> recipes </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-card>
-      <v-card class="flex-column d-flex overflow-hidden" shrink="auto">
-        <v-col>
-          <script
-            async
-            src="https://spendsmart.wpengine.com/calculator/init.js"
-          ></script>
-          <div
-            id="sses-calculator"
-            class="d-flex d-flex-centered"
-            data-calculatordata
-          ></div>
-        </v-col>
-      </v-card>
-      <v-card
-        class="blue-grey lighten-5 flex-colum d-flex justify-start overflow-hidden"
-        justify="start"
-        id="first-card"
-        width="400"
-      >
-        <span class="text-h5 black--text" align="center">
-          <!-- random list of recipes -->
-          <v-icon large color="black"> mdi-all-inclusive </v-icon>
-        </span>
-        <v-spacer></v-spacer>
-        <v-menu bottom left>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn dark icon v-bind="attrs" v-on="on">
-              <v-icon class="black--text">mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item>
-              <v-list-item-title> recipes </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-card>
-    </div>
-    <!-- <v-card v-for="cards in recipeCards"
-  :key="cards"
-  height = "500"
-  width="500"
-  class="d-flex text-white"
-  >
-  <v-title>
-    {{recipeCards.title}}
-  </v-title>
-    <v-subtitle>
-      {{ recipeCards.decription}} @{{ recipeCards.title}}
-    </v-subtitle>
-    <v-card-actions>
-      {{recipeCards.description}}
-    </v-card-actions>
-</v-card> -->
-  </v-main>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
 <style>
-#wheel-txt {
-  color: black;
-}
-#first-card {
-  padding: 0.5em;
-  margin: 0.5em;
-  justify-content: left;
-}
-@media screen and (max-width: 400px){
-    #main-container{
-      display: flex;
-      size: auto;
-      overflow: hidden;
-    }
-    #parallax{
-      display: flex;
-      size: auto;
-      overflow: hidden;
-    }
-    #card-container{
-      display: flex;
-      size: auto;
-      overflow: hidden;
-    }
+@media screen and (max-width: 400px) {
+  #main-container {
+    display: flex;
+    size: auto;
+    overflow: hidden;
+  }
 }
 </style>
 
 <script>
-import { mdiAllInclusiveBox } from '@mdi/js';
+import { mdiAllInclusiveBox, mdiCartOutline } from '@mdi/js'
 export default {
   name: 'IndexPage',
-  layout: 'default',
+  layout: 'none',
   data() {
-  return {
+    return {
       recipeCards: [
-      {
-        title: 'Random Recipes',
-        id: 'Random',
-        description: 'Pick and choose a random recipe from our list of recipes',
+        {
+          title: 'Random Recipes',
+          id: 'Random',
+          description:
+            'Pick and choose a random recipe from our list of recipes',
+        },
+        {
+          title: 'Pick from our selection of recipes',
+          id: 'List',
+          description:
+            'Choose a recipe from our list of recipes, we hope you enjoy!',
+        },
+      ],
+      links: [{
+        name: "Random Recipe List",
+        to: "/weeklycart",
+        id: "ran-rec"
       },
       {
-        title: 'Pick from our selection of recipes',
-        id: 'List',
-        description:
-          'Choose a recipe from our list of recipes, we hope you enjoy!',
-      },
-    ],
-  }
-}
+        name: "Ordered Recipes",
+        to: "/weeklycart",
+        id: "ordered-rec"
+      }]
+    }
+  },
 }
 </script>
