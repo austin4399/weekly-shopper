49386262a0ff6f020cd9c8a15b7150346649ad94
auhallet <austin.hallett-doughty@silabs.com>
2022-03-29 18:22:02 -0400

    changed to bottom navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="flex flex-around grey darken-1"
-      flat
-      rounded
-    >
-      <div
-        :color="
-          $vuetify.breakpoint.smAndDown ? 'light-grey lighten-3' : 'transparent'
-        "
-        size="32"
-      ></div>
-      <v-tabs grow centered class="ml-n9" color="black darken-1">
-        <!-- path to weekly cart -->
-        <v-tab left class="flex flex-between" to="/">
-          <v-icon class="pa-md-sm"> mdi-cart-outline </v-icon>
-        </v-tab>
-        <!-- path to other pages -->
-        <v-tab v-for="link in links" :key="link" :to="link.to">
-          {{ link.name }}
-        </v-tab>
-      </v-tabs>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
+    <v-app-bar app color="grey darken-1">
+      <v-row class="ml-sm">
+        <p>Weekly shopper</p>
+      </v-row>
     </v-app-bar>
 
     <v-main>
-        <Nuxt />
+      <Nuxt />
     </v-main>
-    
+
+    <v-bottom-navigation v-model="tab" :background-color="color" dark shift>
+      <v-btn v-for="link in links" :key="link" :to="link.to">
+        <span>{{link.name}}</span>
+        <v-icon>{{link.icon}}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
-<style>
-
-</style>
+<style></style>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
@@ -47,15 +27,26 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 export default class DefaultLayout extends Vue {
   links: any = [
     {
-      name: 'Build a Cart',
+      name: 'Home',
+      to: '/',
+      icon: 'mdi-home-outline',
+    },
+    {
+      name: 'Recipes',
+      to: '/recipes',
+      icon: 'mdi-food-apple-outline',
+    },
+    {
+      name: 'Cart',
       to: '/weeklycart',
-      id: 'ran-rec',
+      icon: 'mdi-cart-outline',
     }
   ]
-
-  clipped: boolean = false;
-  drawer: boolean = false;
-  fixed: boolean = false;
-
+  color: string = 'grey darken-1'
+  tab: number = 0;
+  clipped: boolean = false
+  drawer: boolean = false
+  fixed: boolean = false
 }
+
 </script>
