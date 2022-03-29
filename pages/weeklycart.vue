1af60ca9129ffb394d0d052f17c83cb6831f3194
auhallet <austin.hallett-doughty@silabs.com>
2022-03-29 18:22:39 -0400

    working on stepper for creating weekly list

--- pages/weeklycart.vue
+++ pages/weeklycart.vue
@@ -1,35 +1,66 @@
 <template>
-    <div>
-         <v-card
-    class="blue-grey lighten-5 flex-colum overflow-hidden"
-    justify="start"
-    id="first-card"
-    width="400"
-  >
-    <v-row
-      class="text-h5 black--text justify-space-between pa-md-10"
-      align="top"
-    >
-      <!-- random list of recipes -->
-      <v-icon large color="black"> mdi-all-inclusive </v-icon>
-      <v-menu bottom left>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn dark icon v-bind="attrs" v-on="on">
-            <v-icon class="black--text">mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
+  <v-stepper style="height: 100%" class="grey lighten-1" v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step :complete="e1 > 1" step="1" color="orange">
+        Pick your meals
+      </v-stepper-step>
 
-        <v-list>
-          <v-list-item>
-            <v-list-item-title> recipes </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-row>
-    <!-- <v-spacer></v-spacer> -->
-   <RandomRecipe></RandomRecipe>
-  </v-card>
-    </div>
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 2" step="2" color="orange">
+        Add additional items
+        <small>Optional</small>
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="3">  </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+          <v-main class="grey lighten-3" style="height: 100%">
+            <v-row class="d-flex justify-center">
+              <p class="black--text">Create a shopping list</p>
+            </v-row>
+            <v-row class="d-flex justify-center">
+              <v-card
+                class="blue-grey lighten-5 flex-column overflow-hidden"
+                id="first-card"
+                max-width="350"
+                min-width="350"
+                min-height="350"
+                max-height="800"
+              >
+                <!-- <v-spacer></v-spacer> -->
+                <RandomRecipe></RandomRecipe>
+              </v-card>
+            </v-row>
+          </v-main>
+
+        <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
+
+        <v-btn text> Cancel </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
+
+        <v-btn text @click="e1 -= 1"> Go back </v-btn>
+
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
+
+        <v-btn text @click="e1 -= 1"> Go back </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
 </template>
 
 <script lang="ts">
@@ -42,5 +73,7 @@ import axios from 'axios'
     RandomRecipe,
   },
 })
-export default class WeeklyCartPage extends Vue {}
+export default class WeeklyCartPage extends Vue {
+  e1: number = 1
+}
 </script>
