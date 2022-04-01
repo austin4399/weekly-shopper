7718f9257418ba3f369727d27334747e36667a7f
mo17817 <lazerharry@gmail.com>
2022-03-31 22:30:17 -0400

    fixed weird display issues

--- pages/index.vue
+++ pages/index.vue
@@ -11,79 +11,39 @@
             <v-img src="https://picsum.photos/1900/1080"> </v-img>
           </v-sheet>
         </v-col>
-      </v-row>
-      <v-row>
         <v-col>
           <v-bottom-sheet v-model="sheet" inset>
             <template v-slot:activator="{ on, attrs }">
-              <v-col align="center">
-    <v-main class="grey lighten-2" style="height: 100%;">
-      <v-container>
-        <v-row class="d-flex justify-center">
-          <p
-            class="text-h5 black--text pa-md-2 ma-lg-5"
-          >Welcome to weekly shopper</p>
-        </v-row>
-        <v-row class="d-flex justify-center">
-          <v-col cols="12" sm="8" align="center">
-            <v-sheet rounded="lg" class="flex flex-center justify-center">
-              <v-img src="https://picsum.photos/1900/1080"> </v-img>
-            </v-sheet>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-bottom-sheet v-model="sheet" inset>
-              <template v-slot:activator="{ on, attrs }">
-                <v-col align="center">
+              <v-col>
+                <v-sheet
+                  class="light text-center"
+                  color="transparent"
+                  backround="translucent"
+                  height="auto"
+                >
                   <v-btn
-                    color="deep-orange darken-1"
-                    dark
+                    color="grey darken-1"
+                    fab
+                    large
                     v-bind="attrs"
                     v-on="on"
                   >
-                  <v-icon>
-                    mdi-calculator
-                  </v-icon>
+                    <v-icon> mdi-calculator </v-icon>
                   </v-btn>
-                </v-col>
-
-              </template>
-              <v-sheet class="text-center" height="auto" persistent>
-                <v-btn
-                  color="grey darken-1"
-                  fab
-                  large
-                  dark
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-icon> mdi-calculator </v-icon>
-                </v-btn>
+                </v-sheet>
               </v-col>
             </template>
-            <v-sheet class="text-center" height="auto" persistent>
-              <v-btn
-                class="mt-6"
-                fab
-                dark
-                large
-                color="error"
-                @click="sheet = !sheet"
-              >
-              </v-btn>
-              <div class="my-3">
-                <script
-                  async
-                  src="https://spendsmart.wpengine.com/calculator/init.js"
-                ></script>
-                <div
-                  id="sses-calculator"
-                  class="sses-calculator"
-                  data-calculatordata
-                ></div>
-              </div>
-            </v-sheet>
+            <div class="my-3">
+              <script
+                async
+                src="https://spendsmart.wpengine.com/calculator/init.js"
+              ></script>
+              <div
+                id="sses-calculator"
+                class="sses-calculator"
+                data-calculatordata
+              ></div>
+            </div>
           </v-bottom-sheet>
         </v-col>
       </v-row>
@@ -95,13 +55,18 @@
   align-content: center;
   display: flex;
 }
+@media screen and (max-width: 399) {
+  #pic-container {
+    font-size: medium;
+  }
+}
 </style>
 
 <script lang="ts">
 import { response } from 'express'
 import axios from 'axios'
 import Vue from 'vue'
-import {Component} from 'vue-property-decorator'
+import { Component } from 'vue-property-decorator'
 @Component({})
 export default class IndexPage extends Vue {
   mounted() {
@@ -123,6 +88,6 @@ async function producePrice() {
   } catch (Error) {
     console.log(Error)
   }
-  sheet: boolean = false;
+  // sheet: boolean = false;
 }
 </script>
