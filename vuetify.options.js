322cb71a4156bde07f0717f10a3dafb453750d58
auhallet <austin.hallett-doughty@silabs.com>
2022-04-12 09:38:20 -0400

    added custom theme module

---
+++ vuetify.options.js
@@ -0,0 +1,26 @@
+export default {
+    theme: {
+        themes: {
+            light: {
+                primary: '#68CDE7',
+                secondary: '#F7F9A7',
+                neutral: '#f2f5f3',
+                accent: '#0086CB',
+                error: '#FF5A5F',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107'
+            },
+            dark: {
+                primary: '#1976D2',
+                secondary: '#424242',
+                accent: '#82B1FF',
+                error: '#FF5A5F',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107'
+            }
+        },
+        
+    }
+}
