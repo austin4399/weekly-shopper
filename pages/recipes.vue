3bebb74084db9e59875c350648660fa61c74a3d7
auhallet <austin.hallett-doughty@silabs.com>
2022-04-12 20:02:07 -0400

    FAB + removed footer / header + clean recipe card

--- pages/recipes.vue
+++ pages/recipes.vue
@@ -1,149 +1,143 @@
 <template>
-  <v-main class="neutral" style="height: 100%">
-    <v-card class="grey lighten-1 pa-sm-2 ma-sm-4">
-      <v-card-title>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-
-      <v-data-iterator :items="recipes" :search="search">
-        <template v-slot:header>
-          <v-toolbar class="mb-2" color="primary" dark flat>
-            <!-- form button -->
-            <v-col class="d-flex flex-between" align="right">
-
-         <v-btn  outlined color="white" fab small class="align-right" rounded @click="dialogState = !dialogState">
-           <v-icon>
-           mdi-plus
-         </v-icon>
-         </v-btn>
-         </v-col>
-            <v-toolbar-title>This is a header</v-toolbar-title>
-            <v-spacer></v-spacer>
-          </v-toolbar>
-        </template>
-
-        <template v-slot:default="props">
-          <v-row class="pa-sm-2">
-            <v-col
-              v-for="recipe in props.items"
-              :key="recipe.title"
-              cols="12"
-              sm="6"
-              md="4"
-              lg="3"
-            >
-              <v-card class="grey lighten-3 black--text">
-                <v-card-title class="text-center subheading font-weight-bold">
-                  {{ recipe.title }}
-                </v-card-title>
-
-                <v-divider></v-divider>
-
-                <v-list dense class="white black--text">
-                  <v-list-item class="black--text">
-                    <v-list-item-content>description:</v-list-item-content>
-                    <v-list-item-content class="align-end black--text">
-                      {{ recipe.description }}
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item class="black--text">
-                    <v-list-item-content>Ingredients:</v-list-item-content>
-                    <v-list-item-content
-                      class="align-end black--text"
-                      v-for="ingredient in recipe.ingredients"
-                      :key="ingredient"
-                    >
-                      <span>
-                        {{ ingredient.name }}
-                      </span>
-
-                      <span>
-                        {{ ingredient.description }}
-                      </span>
-                      <span>
-                        {{ ingredient.type }}
-                      </span>
-                      <span>
-                        {{ ingredient.cost }}
-                      </span>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-col>
-          </v-row>
-        </template>
-        <template v-slot:footer>
-          <v-toolbar class="mt-2" color="primary" dark flat>
-            <v-toolbar-title class="subheading">
-              This is a footer
-            </v-toolbar-title>
-          </v-toolbar>
-        </template>
-      </v-data-iterator>
-      <v-dialog class="ma-sm-2 pa-md-2" v-model="dialogState">
-      <v-card class="ma-sm-2 pa-md-2">
-        <v-card-title class="align-center" align="center">
-          Add a recipe
+  <div>
+    <div v-if="pageLoading == false">
+      <v-card class="grey lighten-2">
+        <v-card-title>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
         </v-card-title>
-        <v-form @submit.prevent="createRecipe()">
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    type="text"
-                    placeholder="Title"
-                    v-model="createRecipeForm.title"
-                    hint="title for your recipe"
-                    required
-                  />
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    type="text"
-                    placeholder="Description"
-                    v-model="createRecipeForm.description"
-                    hint="description for your recipe"
-                    required
-                  />
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field
-                    type="text"
-                    placeholder="Ingredients"
-                    v-model="createRecipeForm.ingredients"
-                    hint="Ingredients for your recipe"
-                    required
-                  />
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field
-                  type="text"
-                  placeholder="Created At"
-                  >
-                  </v-text-field>
-                 </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-spacer></v-spacer>
-          <v-card-actions>
-            <v-btn type="submit"> Submit </v-btn>
-            <v-btn @click="dialogState = !dialogState">Cancel</v-btn>
-          </v-card-actions>
-        </v-form>
+
+        <v-data-iterator
+          class="mb-lg-10"
+          :items="recipes"
+          :search="search"
+          disable-pagination
+          disable-items-per-page
+          hide-default-footer
+        >
+          <template v-slot:default="props">
+            <v-row class="px-md-1 mx-md-1">
+              <v-col
+                v-for="recipe in props.items"
+                :key="recipe.title"
+                cols="12"
+                sm="6"
+                md="4"
+                lg="3"
+              >
+                <v-card class="grey lighten-3 black--text">
+                  <v-card-title class="subheading font-weight-bold pa-md-8">
+                    <v-row>
+                      {{ recipe.title }}
+                      <v-spacer></v-spacer>
+                      <v-btn small rounded fab elevation="1" color="primary">
+                        <v-icon> mdi-pencil </v-icon>
+                      </v-btn>
+                    </v-row>
+                  </v-card-title>
+
+                  <v-divider></v-divider>
+
+                  <v-list dense class="white black--text">
+                    <v-list-item class="black--text">
+                      <v-col>
+                        <v-list-item-content
+                          class="black--text font-weight-thin"
+                        >
+                          {{ recipe.description }}
+                        </v-list-item-content>
+                      </v-col>
+                    </v-list-item>
+
+                    <v-list-item class="black--text">
+                      <v-col class="align-center">
+                        <v-list-item-content class="font-weight-thin">
+                          Ingredients
+                        </v-list-item-content>
+                        <v-list-item-content
+                          class="align-end black--text"
+                          v-for="ingredient in recipe.ingredients"
+                          :key="ingredient"
+                        >
+                          <v-list-item-title>
+                            <v-icon> mdi-circle-small </v-icon>
+                            {{ ingredient.name }}
+                          </v-list-item-title>
+                          <v-list-item-subtitle class="pl-md-5">
+                            {{ ingredient.description }}
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-col>
+                    </v-list-item>
+                  </v-list>
+                </v-card>
+              </v-col>
+            </v-row>
+          </template>
+        </v-data-iterator>
+        <v-dialog class="ma-sm-2 pa-md-2" v-model="dialogState">
+          <v-card class="pa-md-2">
+            <v-card-title class="align-center" align="center">
+              Add a recipe
+            </v-card-title>
+            <v-form @submit.prevent="createRecipe()">
+              <v-card-text>
+                <v-container>
+                  <v-col>
+                    <v-text-field
+                      type="text"
+                      placeholder="Title"
+                      v-model="createRecipeForm.title"
+                      hint="Please give your recipe a title"
+                      required
+                    />
+                    <v-textarea
+                      type="text"
+                      placeholder="Description"
+                      v-model="createRecipeForm.description"
+                      hint="Please give your recipe a description"
+                      required
+                    />
+                    <v-text-field
+                      type="text"
+                      placeholder="Ingredients"
+                      v-model="createRecipeForm.ingredients"
+                      hint="Ingredients for your recipe"
+                      required
+                    />
+                  </v-col>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-row class="ma-sm-2 pa-sm-2 justify-space-between">
+                  <v-btn type="submit"> Submit </v-btn>
+                  <v-btn @click="dialogState = !dialogState">Cancel</v-btn>
+                </v-row>
+              </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-dialog>
       </v-card>
-    </v-dialog>
-    </v-card>
-  </v-main>
+      <v-btn
+        id="floating-action-button"
+        fab
+        bottom
+        right
+        fixed
+        @click="dialogState = true"
+      >
+        <v-icon> mdi-plus </v-icon>
+      </v-btn>
+    </div>
+    <div v-else>
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -169,7 +163,9 @@ type Recipe = {
 export default class RecipesPage extends Vue {
   mounted() {
     this.getRecipe()
+    this.pageLoading = false
   }
+  pageLoading = true
   recipes: any = []
   search: string = ''
   dialogState: boolean = false
@@ -212,5 +208,8 @@ export default class RecipesPage extends Vue {
 <style lang="css">
 #search {
   resize: both;
+}
+#floating-action-button {
+  bottom: 80px;
 }
 </style>
