4d051a17f3becc244c1d773f017ed6a170fb37c7
mo17817 <lazerharry@gmail.com>
2022-04-22 21:21:02 -0400

    messed around with the home page

---
+++ components/HomeParallax.vue
@@ -0,0 +1,166 @@
+<template>
+  <v-main style="padding-top: 0%;">
+    <v-row style="padding-top: 0%;">
+      <v-col class="col" style="padding-top: 0%;">
+        <v-container
+          fluid
+          elevation="20"
+          class="video-container"
+          align="center"
+        >
+          <iframe
+            class="video"
+            src="https://giphy.com/embed/giXLnhxp60zEEIkq8K"
+            frameBorder="0"
+          >
+          </iframe>
+          <v-text
+            elevation="20"
+            class="font-weight-light text d-flex justify-center black--text"
+            align="center"
+          >
+            Weekly shopper
+            <v-icon size="29px"> mdi-cart-outline </v-icon>
+          </v-text>
+        </v-container>
+      </v-col>
+    </v-row>
+
+
+<!-- Tried out Sparkline component
+ <v-row>
+  <v-col>
+    <v-card
+    class="mx-auto text-center"
+    color="secondary"
+    dark
+    max-width="600"
+    style="padding-top: 0%; margin-top: 0%;"
+  >
+    <v-card-text>
+      <v-sheet color="grey darken-3">
+        <v-sparkline
+          :value="value"
+          color="rgba(255, 255, 255, .7)"
+          height="100"
+          padding="24"
+          stroke-linecap="round"
+          smooth
+        >
+          <template v-slot:label="item">
+            {{ item.value }}
+          </template>
+        </v-sparkline>
+      </v-sheet>
+    </v-card-text>
+
+    <v-card-text>
+      <div class="text-h4 font-weight-thin grey--text">
+        Total Recipes
+      </div>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-actions class="justify-center grey--text">
+      <v-btn
+        block
+        text
+        to="/recipes"
+        class="grey--text"
+      >
+        check them out
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+      </v-col>
+    </v-row> -->
+
+    <v-col>
+      <v-bottom-sheet v-model="sheet" inset>
+        <template v-slot:activator="{ on, attrs }">
+          <v-col>
+            <v-sheet
+              class="light text-center"
+              color="transparent"
+              backround="translucent"
+              height="auto"
+              style="padding-top: 0%;"
+            >
+              <v-btn color="grey darken-1" fab large v-bind="attrs" v-on="on">
+                <v-icon> mdi-calculator </v-icon>
+              </v-btn>
+            </v-sheet>
+          </v-col>
+        </template>
+        <div class="my-3">
+          <script
+            async
+            src="https://spendsmart.wpengine.com/calculator/init.js"
+          ></script>
+          <div
+            id="sses-calculator"
+            class="sses-calculator"
+            data-calculatordata
+          ></div>
+        </div>
+      </v-bottom-sheet>
+    </v-col>
+  </v-main>
+</template>
+
+<script lan="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import axios from 'axios';
+@Component({
+  components: {},
+  mounted(){
+    getRecipes()
+  }
+})
+export default class HomeParallax extends Vue {
+  value =
+   [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]
+
+}
+async function getRecipes(){
+  try{
+  const endpoint = '/api/v1/recipes'
+  const recipeCall = await axios.get(endpoint)
+  const totalRecipes = every(recipeCall)
+}
+  catch(error){}
+}
+</script>
+
+<style>
+.video-container {
+  position: relative;
+  height: 50vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-right: 3%;
+}
+.video {
+  position: absolute;
+  width: auto;
+  height: auto;
+  min-width: 100%;
+  min-height: 100%;
+  top: 50%;
+  left: 50%;
+  bottom: 20%;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
+  align-items: start;
+  padding-top: 0%
+}
+.text {
+  z-index: 1;
+  padding-top: 28px;
+  font-size: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,24 @@
 <template>
-  <v-main align="center" class="grey lighten-3" style="height: 100%">
-    <v-container>
-      <v-row
-        align-content-lg="center"
-        class="flex flex-center"
-        id="pic-container"
-      >
-        <v-col class="align-content-lg" cols="12" sm="8" align="center">
-          <v-sheet rounded="lg" class="flex flex-center justify-center">
-            <v-img src="https://picsum.photos/1900/1080"> </v-img>
-          </v-sheet>
-        </v-col>
-        <v-col>
-          <v-bottom-sheet v-model="sheet" inset>
-            <template v-slot:activator="{ on, attrs }">
-
-              <v-col>
-                <v-sheet
-                  class="light text-center"
-                  color="transparent"
-                  backround="translucent"
-                  height="auto"
-                >
-                  <v-btn
-                    color="grey darken-1"
-                    fab
-                    large
-                    v-bind="attrs"
-                    v-on="on"
-                  >
-                    <v-icon> mdi-calculator </v-icon>
-                  </v-btn>
-                </v-sheet>
-              </v-col>
-            </template>
-            <div class="my-3">
-              <script
-                async
-                src="https://spendsmart.wpengine.com/calculator/init.js"
-              ></script>
-              <div
-                id="sses-calculator"
-                class="sses-calculator"
-                data-calculatordata
-              ></div>
-            </div>
-          </v-bottom-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-main
+    align="center"
+    class="main-container"
+    style="height: 100vh; position: relative; padding-top: 0%;"
+  >
+  <HomeParallax> </HomeParallax>
   </v-main>
 </template>
 <style>
+.main-container {
+  position: relative;
+  height: 50vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  padding-right: 3%;
+  padding-top: 0%
+}
 #pic-container {
   align-content: center;
   display: flex;
@@ -68,9 +35,14 @@ import { response } from 'express'
 import axios from 'axios'
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-@Component({})
+import HomeParallax from '../components/HomeParallax.vue'
+@Component({
+  components: {
+    HomeParallax,
+  },
+})
 export default class IndexPage extends Vue {
-  sheet: boolean = false;
+  sheet: boolean = false
   mounted() {
     produceCalories()
   }
@@ -80,25 +52,26 @@ export default class IndexPage extends Vue {
 // still waiting on new api-key
 async function produceCalories() {
   try {
-    const res = await axios.get("https://api.edamam.com/api/food-database/v2/parser", {
-      params: {
-        app_id : 'a477c607',
-        app_key :'b3d03a0454201b92de94a5a6165da6bc',
-        ingr : 'bacon',
-        nutritiontype : 'logging',
-        category: 'generic-foods',
-        calories: '0-700'
-    },
-      data: {
-      },
-      headers: {
-        'Accept': 'application/json',
-        'Access-Control': 'Allow-Origin',
-        'status':200
-      },
-    }).then(data => {
-      console.log(data);
-    })
+    const res = await axios
+      .get('https://api.edamam.com/api/food-database/v2/parser', {
+        params: {
+          app_id: 'a477c607',
+          app_key: 'b3d03a0454201b92de94a5a6165da6bc',
+          ingr: 'bacon',
+          nutritiontype: 'logging',
+          category: 'generic-foods',
+          calories: '0-700',
+        },
+        data: {},
+        headers: {
+          Accept: 'application/json',
+          'Access-Control': 'Allow-Origin',
+          status: 200,
+        },
+      })
+      .then((data) => {
+        console.log(data)
+      })
   } catch (Error) {
     console.log(Error)
   }
