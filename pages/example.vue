6bf12a42877b0a351f2d585a501b4628877c4c77
auhallet <austin.hallett-doughty@silabs.com>
2022-05-14 11:34:54 -0400

    added some formatting for easier reading

--- pages/example.vue
+++ pages/example.vue
@@ -1,31 +1,40 @@
 <template>
-  <div class="pa-md-10 mx-md-10">
-    <v-row>
-      <div>
-        <v-btn @click="CLOSE">CLOSE</v-btn>
-      </div>
-      <div>
-        {{ recipeFormDialog }}
-      </div>
-      <div>
-        <v-btn @click="OPEN">OPEN</v-btn>
-      </div>
-    </v-row>
-    <v-row
-      v-for="item in cart"
-      :key="item"
-    >
-      {{item}}
-    </v-row>
+  <div class="pa-md-10 ma-md-10">
+    <div>
+      <v-row>
+        <h3>The below element is pulled from store index</h3>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn @click="CLOSE">CLOSE</v-btn>
+        </v-col>
+        <v-col>
+          {{ recipeFormDialog }}
+        </v-col>
+        <v-col>
+          <v-btn @click="OPEN">OPEN</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+    <div>
+      <v-row>
+        <v-col>
+          <h3>The below element is pulled from store cart module</h3>
+          <v-row v-for="item in cart" :key="item" class="mt-sm-5">
+            {{ item }}
+          </v-row>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { mapMutations, mapState, mapActions, mapGetters } from 'vuex';
-import { State, Mutation, Action, namespace } from 'vuex-class';
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { mapMutations, mapState, mapActions, mapGetters } from 'vuex'
+import { State, Mutation, Action, namespace } from 'vuex-class'
 
-const cartModule = namespace('cart');
+const cartModule = namespace('cart')
 
 @Component({
   methods: {
@@ -33,8 +42,8 @@ const cartModule = namespace('cart');
   },
 })
 export default class ExamplePage extends Vue {
-  @State recipeFormDialog!: boolean;
+  @State recipeFormDialog!: boolean
 
-  @cartModule.State cart!: string[];
+  @cartModule.State cart!: string[]
 }
 </script>
