9fc5fb6ac87452f9ba44d35176e0749f2d1b8236
auhallet <austin.hallett-doughty@silabs.com>
2022-05-24 18:03:57 -0400

    formatting + CSS

--- components/subcomponents/Alert.vue
+++ components/subcomponents/Alert.vue
@@ -1,26 +1,35 @@
 <template>
-    <v-row v-if="alert == true" id="success-alert">
-      <v-alert
-        type="success"
-      >
-        {{ alertMessage }}
-      </v-alert>
-    </v-row>
+  <v-row v-if="alert == true" id="success-alert">
+    <v-alert type="success">
+      {{ alertMessage }}
+    </v-alert>
+  </v-row>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+import { State } from 'vuex-class'
 
-import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
-import {State} from 'vuex-class';
-
-@Component({
-})
+@Component({})
 export default class Alert extends Vue {
-    @State alert!: boolean;
-    @State alertMessage! :string;
-    @Watch('alert')
-    onStateChange(newValue: boolean): void {
-        setTimeout(() => {  this.$store.commit('HIDE_ALERT')}, 3000);
-    }
+  @State alert!: boolean
+  @State alertMessage!: string
+  @Watch('alert')
+  onStateChange(newValue: boolean): void {
+    setTimeout(() => {
+      this.$store.commit('HIDE_ALERT')
+    }, 3000)
+  }
 }
 </script>
+
+<style scoped>
+#success-alert {
+  width: fit-content;
+  position: fixed;
+  top: 10%;
+  right: 50%;
+  left: 50%;
+  z-index: 9999;
+}
+</style>
