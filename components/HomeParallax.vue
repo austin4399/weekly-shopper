12bc62778bff6e835bce0998a07745eac0dfbb74
mo17817 <lazerharry@gmail.com>
2022-05-27 15:29:18 -0400

    created btn that gets random recipe

--- components/HomeParallax.vue
+++ components/HomeParallax.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-main style="padding-top: 0%;">
-    <v-row style="padding-top: 0%;">
-      <v-col class="col" style="padding-top: 0%;">
+  <v-main style="padding-top: 0%">
+    <v-row style="padding-top: 0%">
+      <v-col class="col" style="padding-top: 0%">
         <v-container
           fluid
           elevation="20"
@@ -25,57 +25,6 @@
         </v-container>
       </v-col>
     </v-row>
-
-
-<!-- Tried out Sparkline component
- <v-row>
-  <v-col>
-    <v-card
-    class="mx-auto text-center"
-    color="secondary"
-    dark
-    max-width="600"
-    style="padding-top: 0%; margin-top: 0%;"
-  >
-    <v-card-text>
-      <v-sheet color="grey darken-3">
-        <v-sparkline
-          :value="value"
-          color="rgba(255, 255, 255, .7)"
-          height="100"
-          padding="24"
-          stroke-linecap="round"
-          smooth
-        >
-          <template v-slot:label="item">
-            {{ item.recs }}
-          </template>
-        </v-sparkline>
-      </v-sheet>
-    </v-card-text>
-
-    <v-card-text>
-      <div class="text-h4 font-weight-thin grey--text">
-        Total Recipes
-      </div>
-    </v-card-text>
-
-    <v-divider></v-divider>
-
-    <v-card-actions class="justify-center grey--text">
-      <v-btn
-        block
-        text
-        to="/recipes"
-        class="grey--text"
-      >
-        check them out
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-      </v-col>
-    </v-row> -->
-
     <v-col>
       <v-bottom-sheet v-model="sheet" inset>
         <template v-slot:activator="{ on, attrs }">
@@ -85,10 +34,12 @@
               color="transparent"
               backround="translucent"
               height="auto"
-              style="padding-top: 0%;"
+              style="padding-top: 0%"
             >
               <v-btn color="secondary" fab large v-bind="attrs" v-on="on">
-                <v-icon class="grey--text" style="backround: grey darken-3;"> mdi-calculator </v-icon>
+                <v-icon class="grey--text" style="backround: grey darken-3">
+                  mdi-calculator
+                </v-icon>
               </v-btn>
             </v-sheet>
           </v-col>
@@ -105,22 +56,117 @@
           ></div>
         </div>
       </v-bottom-sheet>
+      <v-spacer></v-spacer>
+      <v-row>
+        <v-col>
+          <v-btn
+            round
+            color="secondary"
+            class="secondary"
+            @click="activator = true"
+          >
+            <v-icon class="grey--text" style="backround: grey darken-3">
+              mdi-help-circle
+            </v-icon>
+          </v-btn>
+          <v-btn
+            class="secondary"
+            color="secondary"
+            v-show="activator == true"
+            @click="activator = false"
+          >
+            <v-icon class="grey--text" style="backround: grey darken-3"> mdi-cancel </v-icon>
+          </v-btn>
+          <v-card
+            class="secondary black--text font-weight-light"
+            v-show="activator == true"
+            v-for="objects in recipeData"
+            :key="objects"
+            style="height: 90%"
+            id="card"
+          >
+            <v-card-title class="subheading font-weight-light pa-md-8">
+              <v-row>
+                title:
+                {{ objects.title }}
+              </v-row>
+            </v-card-title>
+
+            <v-divider></v-divider>
+
+            <v-list dense class="white black--text">
+              <v-list-item class="black--text">
+                <v-col>
+                  <v-list-item-content class="black--text font-weight-thin">
+                    Recipe description:
+                    {{ objects.description }}
+                  </v-list-item-content>
+                </v-col>
+              </v-list-item>
+
+              <v-list-item class="black--text">
+                <v-col class="align-center">
+                  <v-list-item-content class="font-weight-thin">
+                    Ingredients:
+                  </v-list-item-content>
+                  <v-list-item-content
+                    class="align-end black--text"
+                    v-for="(ingredient, index) in objects.ingredients"
+                    :key="index"
+                  >
+                    <v-list-item-title>
+                      <v-icon> mdi-circle-small </v-icon>
+                      name:
+                      {{ ingredient.name }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle class="pl-md-5">
+                      description:
+                      {{ ingredient.description }}
+                    </v-list-item-subtitle>
+                    <v-list-subtitle>
+                      type:
+                      {{ ingredient.type }}
+                    </v-list-subtitle>
+                    <v-list-subtitle>
+                      cost:
+                      {{ ingredient.cost }}
+                    </v-list-subtitle>
+                  </v-list-item-content>
+                </v-col>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-col>
   </v-main>
 </template>
 
-<script lan="ts">
+<script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import axios from 'axios';
+import axios from 'axios'
 @Component({
   components: {},
-  mounted(){
-
-  }
 })
 export default class HomeParallax extends Vue {
-  value =
-   [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]
+  recipeData: Array<any> = []
+  activator: boolean = false
+  sheet: boolean = false
+  mounted() {
+    this.randomRecipe()
+  }
+
+  async randomRecipe(): Promise<void> {
+    try {
+      const response = await axios.get('api/v1/random')
+      const randomHolder = response.data
+      this.recipeData.push(randomHolder[0])
+      if (response.status == 200) {
+        console.log('sucess!')
+      }
+      console.log(this.recipeData)
+    } catch (error) {}
+  }
 }
 </script>
 
@@ -147,7 +193,7 @@ export default class HomeParallax extends Vue {
   transform: translate(-50%, -50%);
   pointer-events: none;
   align-items: start;
-  padding-top: 0%
+  padding-top: 0%;
 }
 .text {
   z-index: 1;
